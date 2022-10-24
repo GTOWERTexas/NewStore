24bedb3ca6519a852d912f0994cb57d59a9229fe
GTowerTexas <96723535+GTOWERTexas@users.noreply.github.com>
2022-10-24 21:31:21 +0300

    Update Cart.cs

--- NewStore/Models/Cart.cs
+++ NewStore/Models/Cart.cs
@@ -3,11 +3,12 @@ using System.Linq;
 
 
 namespace NewStore.Models
-{
+{ 
+    // реализация средства корзины    
     public class Cart
     {
         public List<CartLine> Lines { get; set; } = new List<CartLine>();
-        public virtual void AddItem(Product product, int quantity)
+        public virtual void AddItem(Product product, int quantity) // добавить продукт в колличестве n
         {
             CartLine line = Lines.Where(p=>p.Product.ProductId == product.ProductId).FirstOrDefault();
             if (line == null)
@@ -19,12 +20,12 @@ namespace NewStore.Models
                 line.Quantity += quantity;
             }
         }
-        public virtual void RemoveLine(Product product) => Lines.RemoveAll(l=>l.Product.ProductId == product.ProductId);
-        public decimal ComputeTotalValue() => Lines.Sum(l=>l.Product.Price * l.Quantity);
+        public virtual void RemoveLine(Product product) => Lines.RemoveAll(l=>l.Product.ProductId == product.ProductId); // удалить товар с корзины
+        public decimal ComputeTotalValue() => Lines.Sum(l=>l.Product.Price * l.Quantity); // сумма заказа
         public virtual void Clear() => Lines.Clear();
     }
 
-    public class CartLine
+    public class CartLine // товар, выбранный пользователем
     {
         public int CartLineId { get; set; }
         public Product Product { get; set; }
