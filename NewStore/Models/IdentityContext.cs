e4a80997eab8982b37ddf6b16c2387baddc9e223
GTowerTexas <96723535+GTOWERTexas@users.noreply.github.com>
2022-10-24 21:43:14 +0300

    Update IdentityContext.cs

--- NewStore/Models/IdentityContext.cs
+++ NewStore/Models/IdentityContext.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 
 
 namespace NewStore.Models
-{
+{   // контекст данных о пользователях и их ролях
     public class IdentityContext : IdentityDbContext<IdentityUser>
     {
         public IdentityContext(DbContextOptions<IdentityContext> options) : base(options)
