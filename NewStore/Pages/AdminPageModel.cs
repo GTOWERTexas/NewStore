1e2ad343ed51a0ac5b737b75b5cc90f63b688863
GTowerTexas <96723535+GTOWERTexas@users.noreply.github.com>
2022-10-24 23:05:37 +0300

    Update AdminPageModel.cs

--- NewStore/Pages/AdminPageModel.cs
+++ NewStore/Pages/AdminPageModel.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Authorization;
 namespace NewStore.Pages
-{
+{   // класс предназначен для того, чтобы от него наследовались модели представления Pages доступные только для администратора
     [Authorize(Roles ="Admins")]
     public class AdminPageModel : PageModel
     {
