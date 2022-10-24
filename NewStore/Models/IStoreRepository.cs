c89b590d6be6a7dd83514123800eb49570fab2bd
GTowerTexas <96723535+GTOWERTexas@users.noreply.github.com>
2022-10-24 21:42:30 +0300

    Update IStoreRepository.cs

--- NewStore/Models/IStoreRepository.cs
+++ NewStore/Models/IStoreRepository.cs
@@ -6,13 +6,14 @@ using System.Threading.Tasks;
 namespace NewStore.Models
 {
     public interface IStoreRepository
-    {
+    {   // получение последовательности объектов Product
         IQueryable<Product> Products { get; }
-       
+        // CRUD 
         void CreateProduct(Product p);
         void DeleteProduct(Product p);
         void SaveProduct(Product p);
-
+        
+         // получение последовательности объектов Category
         IQueryable<Category> Categories { get; }
         void CreateCategory(Category c);
         void DeleteCategory(Category c);
