81e0dd84c48af14a1050d1c0efd2f473c6a2f75e
GTowerTexas <96723535+GTOWERTexas@users.noreply.github.com>
2022-10-24 21:41:36 +0300

    Update IOrderRepository.cs

--- NewStore/Models/IOrderRepository.cs
+++ NewStore/Models/IOrderRepository.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace NewStore.Models
-{
+{   
     public interface IOrderRepository
     {
+        // получение последовательности объектов Order
         IQueryable<Order> Orders { get; }
         void SaveOrder(Order order);
     }
