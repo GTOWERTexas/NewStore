96efe19e67bf2c9d75af9b5017dbb6c78f7237a8
GTowerTexas <96723535+GTOWERTexas@users.noreply.github.com>
2022-10-24 20:27:52 +0300

    Update UrlExtensions.cs

--- NewStore/Infrastructure/UrlExtensions.cs
+++ NewStore/Infrastructure/UrlExtensions.cs
@@ -2,7 +2,7 @@
 
 
 namespace NewStore.Infrastructure
-{
+{   // расширение. Хранит обратный путь
     public static class UrlExtensions
     {
         public static string PathEndQuery(this HttpRequest request) => request.QueryString.HasValue ? $"{request.Path} {request.QueryString}" : request.Path.ToString();
