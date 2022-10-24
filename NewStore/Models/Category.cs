35df82bec2310b16cd92bbb096fc45acec6258d0
GTowerTexas <96723535+GTOWERTexas@users.noreply.github.com>
2022-10-24 21:32:42 +0300

    Update Category.cs

--- NewStore/Models/Category.cs
+++ NewStore/Models/Category.cs
@@ -4,15 +4,15 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace NewStore.Models
-{
+{   // класс категорий 
     public class Category
     {
-        public string Name { get; set; }
+        public string Name { get; set; } // имя
 
-        public long Id { get; set; }
+        public long Id { get; set; } // идентификатор
 
-        public IEnumerable<Product> Products { get; set; }
+        public IEnumerable<Product> Products { get; set; } // отношение один ко многим (категрия - продукты)
 
-        public bool IsClothes { get; set; } = false;
+        public bool IsClothes { get; set; } = false; 
     }
 }
