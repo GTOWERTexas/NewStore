a7f549b7d26c8005fe6595be7fa4350b5e60fcca
GTowerTexas <96723535+GTOWERTexas@users.noreply.github.com>
2022-10-24 22:41:31 +0300

    Update IdentitySeedData.cs

--- NewStore/Models/IdentitySeedData.cs
+++ NewStore/Models/IdentitySeedData.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 namespace NewStore.Models
 {
     public class IdentitySeedData
-    {
+    {   // заполнение БД Identity начальными данными
         public static void CreateAdminAccount(IServiceProvider serviceProvider, IConfiguration configuration)
         {
             CreateAdminAccountAsync(serviceProvider, configuration).Wait();
@@ -15,14 +15,15 @@ namespace NewStore.Models
         public static async Task CreateAdminAccountAsync(IServiceProvider serviceProvider, IConfiguration configuration)
         {
             serviceProvider = serviceProvider.CreateScope().ServiceProvider;
-            UserManager<IdentityUser> userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
-            RoleManager<IdentityRole> roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-
+            UserManager<IdentityUser> userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>(); // добавление сервиса User
+            RoleManager<IdentityRole> roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();  // сервис ролей
+              
+            // данные администратора приложения
             string username = configuration["Data:AdminUser:Name"] ?? "admin";
             string email = configuration["Data:AdminUser:Email"] ?? "adminG@example.com";
             string password = configuration["Data:AdminUser:Password"] ?? "secretG456*";
             string role = configuration["Data:AdminUser:Role"] ?? "Admins";
-
+            // создание пользователя с правами администратора и сохранение его в базе данных
             if(await userManager.FindByNameAsync(username) == null)
             {
                 if(await roleManager.FindByNameAsync(role) == null)
