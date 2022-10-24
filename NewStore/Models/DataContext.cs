1bc07822403e14bbca5ff4abbbbc088a5be24643
GTowerTexas <96723535+GTOWERTexas@users.noreply.github.com>
2022-10-24 21:35:52 +0300

    Update DataContext.cs

--- NewStore/Models/DataContext.cs
+++ NewStore/Models/DataContext.cs
@@ -5,14 +5,14 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 
 namespace NewStore.Models
-{
+{   // класс контекста базы данных - посредник между приложением и БД
     public class DataContext:DbContext
     {
         public DataContext(DbContextOptions<DataContext> opts):base(opts)
         {
 
         }
-
+        // эти свойтва предоставляют доступ к объектам в базе данных
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Order> Orders { get; set; }
